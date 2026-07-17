c63617e68624657cf73feadcddfa28071939c551
PrajapatiVikrant <prajapati865039@gmail.com>
2026-07-17 21:36:26 +0530

    remove debuging print

--- utils/helper.py
+++ utils/helper.py
@@ -6,9 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def load_json(filename):
     file_path = os.path.join(BASE_DIR, "knowledge", filename)
 
-    print("BASE_DIR :", BASE_DIR)
-    print("FILE_PATH:", file_path)
-    print("Exists   :", os.path.exists(file_path))
+  
 
     with open(file_path, "r", encoding="utf-8") as f:
         return json.load(f)
